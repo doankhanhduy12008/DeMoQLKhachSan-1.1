4c8b8291711e96ceef6355b663f4bde54734665e
Zinzang823 <ductrung1232013@gmail.com>
2025-07-31 09:36:51 +0700

    Up 4.2

--- src/main/java/Dao/dao/KhachHangDao.java
+++ src/main/java/Dao/dao/KhachHangDao.java
@@ -4,6 +4,7 @@ import Dao.entity.KhachHang;
 import java.util.List;
 
 public interface KhachHangDao extends CrudDao<KhachHang, Integer> {
-    KhachHang findBySDT(String sdt);
+     KhachHang findBySDT(String sdt);
     List<KhachHang> searchBySDT(String sdt);
+    List<KhachHang> searchByCMT(String cmt);
 }
